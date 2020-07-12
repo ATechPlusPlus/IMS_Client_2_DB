66713d0da904c622f8cd907c0385f38a4ab9ecca
AAMIR <AAMIR@KHAN>
2020-07-12 18:36:14 +0530

    1. Removed Primary key from tables becuase mulitple added primary key in same table.

--- IMS_Client_2_DB/Tables/tblCashReturn.sql
+++ IMS_Client_2_DB/Tables/tblCashReturn.sql
@@ -7,10 +7,6 @@
 	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_tblCashReturn_CreatedOn]  DEFAULT (getdate()),
 	[UpdatedBy] [int] NULL,
 	[UpdatedOn] [datetime] NULL
-PRIMARY KEY CLUSTERED 
-(
-	[CashReturnID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
--- IMS_Client_2_DB/Tables/tblCreditClosing.sql
+++ IMS_Client_2_DB/Tables/tblCreditClosing.sql
@@ -9,10 +9,6 @@
 	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_tblCreditClosing_CreatedOn]  DEFAULT (getdate()),
 	[UpdatedBy] [int] NULL,
 	[UpdatedOn] [datetime] NULL
-PRIMARY KEY CLUSTERED 
-(
-	[CreditClosingID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO

c8be064fb592fb9765190a1e3d040fd62b2de3ff
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-07-15 11:14:03 +0530

    Changes for store tranfer

---
+++ IMS_Client_2_DB/Tables/tblStoreTransferBillDetails.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tblStoreTransferBillDetails](
+	[StoreTransferID] [int] NOT NULL IDENTITY,
+	[BillNo] [nvarchar](50) NULL,
+	[FromStore] [int] NULL,
+	[ToStore] [int] NULL,
+	TotalQTY [int] NULL,
+	[BillStatus] [nvarchar](50) NULL,
+	[BillDate] [date] NULL,
+	[Createdon] [datetime] NULL,
+	[CreatedBy] [int] NULL,
+	[ModifedOn] [datetime] NULL,
+	[ModifiedBy] [int] NULL,
+ CONSTRAINT [PK_tblStoreTransferBillDetails] PRIMARY KEY CLUSTERED 
+(
+	[StoreTransferID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+

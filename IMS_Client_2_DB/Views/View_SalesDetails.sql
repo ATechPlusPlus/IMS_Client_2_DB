51825c433b970b5a522d16c9f5542953c93e22ce
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-07-17 20:51:19 +0530

    column added into view

--- IMS_Client_2_DB/Views/View_SalesDetails.sql
+++ IMS_Client_2_DB/Views/View_SalesDetails.sql
@@ -1,7 +1,10 @@
 ﻿CREATE VIEW [dbo].[View_SalesDetails]
 	AS 
-    SELECT        s1.InvoiceID, p1.ProductName, s1.QTY, s1.Rate, sm.Size, c1.ColorName
+      SELECT        s1.InvoiceID, p1.ProductID, ps.BarcodeNo,
+	 p1.ProductName, s1.QTY, s1.Rate, sm.Size, c1.ColorName
 FROM            dbo.SalesDetails AS s1 INNER JOIN
                          dbo.ProductMaster AS p1 ON s1.ProductID = p1.ProductID INNER JOIN
                          dbo.SizeMaster AS sm ON sm.SizeID = s1.SizeID INNER JOIN
                          dbo.ColorMaster AS c1 ON c1.ColorID = s1.ColorID
+						 inner join ProductStockColorSizeMaster ps on s1.ProductID=ps.ProductID and s1.ColorID=ps.ColorID and s1.SizeID=ps.SizeID
+

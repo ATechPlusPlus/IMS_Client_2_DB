e71f67876b2535266fbd3d3dcaca931c83062f59
AAMIR <AAMIR@KHAN>
2020-07-19 21:17:39 +0530

    1. Changes in SP SPR_Get_Productdetails for violetcolor. 2. Chamges in SP Get_StoreTransfer List. 3. Changes in SP Get_Store_Transfer_ListItems.

--- IMS_Client_2_DB/Stores Procedures/SPR_Get_StoreTransfer_List.sql
+++ IMS_Client_2_DB/Stores Procedures/SPR_Get_StoreTransfer_List.sql
@@ -1,42 +1,126 @@
 ﻿-- =============================================
 -- Author:		<AAMIR KHAN>
 -- Create date: <16th JULY 2020>
+-- Update date: <19th JULY 2020>
 -- Description:	<>
 -- =============================================
---EXEC SPR_Get_StoreTransfer_List
+--EXEC SPR_Get_StoreTransfer_List '2020-07-15','2020-07-20',-1
 CREATE PROCEDURE [dbo].[SPR_Get_StoreTransfer_List]
---@FromDate date=0
---,@ToDate date=0
---,@BillStatus VARCHAR(50)='0'
+@FromDate DATE=NULL
+,@ToDate DATE=NULL
+,@BillStatus INT=NULL  -- @BillStatus < 0 means show all data
 AS
 BEGIN
 
 	SET NOCOUNT ON;
 	
 	DECLARE @PARAMERES VARCHAR(MAX)=''
-	DECLARE @StoreID AS INT=0
+	--DECLARE @StoreID AS INT=0
 
 	BEGIN TRY
 
-	SELECT @StoreID=StoreID FROM DefaultStoreSetting WITH(NOLOCK) WHERE MachineName=HOST_NAME()
-	
+	SET @PARAMERES=CONCAT(@FromDate,',',@ToDate,',',@BillStatus)
+
+	--SELECT @StoreID=StoreID FROM DefaultStoreSetting WITH(NOLOCK) WHERE MachineName=HOST_NAME()
+	IF @BillStatus < 0
+	BEGIN
+
 	SELECT stb.StoreTransferID,stb.BillNo,stb.BillDate,sm.StoreName [FromBranch]
 	,sm1.StoreName [ToBranch],stb.TotalQTY
 	,(CASE WHEN [str].StoreTransferID IS NULL THEN 'Not Received' ELSE 'Received' END) [Received State]
-	,stb.CreatedBy,usr.UserName [TransferedBy],usr1.UserName [ReceivedBy]
+	,stb.CreatedBy,usr.UserName [TransferedBy],usr1.UserName [ReceivedBy],[str].ReceiveBillDate
 	FROM tblStoreTransferBillDetails stb
 	INNER JOIN StoreMaster sm ON stb.FromStore=sm.StoreID
 	INNER JOIN 
 	(
 		SELECT StoreID,StoreName FROM StoreMaster WITH(NOLOCK)
-	)sm1 ON stb.ToStore=sm1.StoreID AND stb.ToStore=@StoreID
-
+	)sm1 ON stb.ToStore=sm1.StoreID --AND stb.ToStore=@StoreID
+	
 	INNER JOIN UserManagement usr ON stb.CreatedBy=usr.UserID
 	LEFT OUTER JOIN tblStoreTransferReceiveBillDetails [str] ON stb.StoreTransferID=[str].StoreTransferID
 	LEFT OUTER JOIN
 	(
 		SELECT UserID,UserName FROM UserManagement WITH(NOLOCK)
 	) usr1 ON [str].CreatedBy=usr1.UserID
+	ORDER BY stb.BillDate
+
+	END
+
+	ELSE
+	BEGIN
+		IF @FromDate IS NULL AND @BillStatus IS NULL
+		BEGIN
+	
+			SELECT Top 100 stb.StoreTransferID,stb.BillNo,stb.BillDate,sm.StoreName [FromBranch]
+			,sm1.StoreName [ToBranch],stb.TotalQTY
+			,(CASE WHEN [str].StoreTransferID IS NULL THEN 'Not Received' ELSE 'Received' END) [Received State]
+			,stb.CreatedBy,usr.UserName [TransferedBy],usr1.UserName [ReceivedBy],[str].ReceiveBillDate
+			FROM tblStoreTransferBillDetails stb
+			INNER JOIN StoreMaster sm ON stb.FromStore=sm.StoreID
+			INNER JOIN 
+			(
+				SELECT StoreID,StoreName FROM StoreMaster WITH(NOLOCK)
+			)sm1 ON stb.ToStore=sm1.StoreID --AND stb.ToStore=@StoreID
+
+			INNER JOIN UserManagement usr ON stb.CreatedBy=usr.UserID
+			LEFT OUTER JOIN tblStoreTransferReceiveBillDetails [str] ON stb.StoreTransferID=[str].StoreTransferID
+			LEFT OUTER JOIN
+			(
+				SELECT UserID,UserName FROM UserManagement WITH(NOLOCK)
+			) usr1 ON [str].CreatedBy=usr1.UserID
+			ORDER BY stb.BillDate DESC
+
+		END
+
+		ELSE IF @FromDate IS NOT NULL AND @BillStatus IS NULL
+		BEGIN
+	
+			SELECT stb.StoreTransferID,stb.BillNo,stb.BillDate,sm.StoreName [FromBranch]
+			,sm1.StoreName [ToBranch],stb.TotalQTY
+			,(CASE WHEN [str].StoreTransferID IS NULL THEN 'Not Received' ELSE 'Received' END) [Received State]
+			,stb.CreatedBy,usr.UserName [TransferedBy],usr1.UserName [ReceivedBy],[str].ReceiveBillDate
+			FROM tblStoreTransferBillDetails stb
+			INNER JOIN StoreMaster sm ON stb.FromStore=sm.StoreID
+			INNER JOIN 
+			(
+				SELECT StoreID,StoreName FROM StoreMaster WITH(NOLOCK)
+			)sm1 ON stb.ToStore=sm1.StoreID --AND stb.ToStore=@StoreID
+
+			INNER JOIN UserManagement usr ON stb.CreatedBy=usr.UserID
+			LEFT OUTER JOIN tblStoreTransferReceiveBillDetails [str] ON stb.StoreTransferID=[str].StoreTransferID
+			LEFT OUTER JOIN
+			(
+				SELECT UserID,UserName FROM UserManagement WITH(NOLOCK)
+			) usr1 ON [str].CreatedBy=usr1.UserID
+			WHERE stb.BillDate BETWEEN @FromDate AND @ToDate
+
+		END
+
+		ELSE IF @FromDate IS NULL AND @BillStatus IS NOT NULL
+		BEGIN
+	
+			SELECT stb.StoreTransferID,stb.BillNo,stb.BillDate,sm.StoreName [FromBranch]
+			,sm1.StoreName [ToBranch],stb.TotalQTY
+			,(CASE WHEN [str].StoreTransferID IS NULL THEN 'Not Received' ELSE 'Received' END) [Received State]
+			,stb.CreatedBy,usr.UserName [TransferedBy],usr1.UserName [ReceivedBy],[str].ReceiveBillDate
+			FROM tblStoreTransferBillDetails stb
+			INNER JOIN StoreMaster sm ON stb.FromStore=sm.StoreID
+			INNER JOIN 
+			(
+				SELECT StoreID,StoreName FROM StoreMaster WITH(NOLOCK)
+			)sm1 ON stb.ToStore=sm1.StoreID --AND stb.ToStore=@StoreID
+
+			INNER JOIN UserManagement usr ON stb.CreatedBy=usr.UserID
+			LEFT OUTER JOIN tblStoreTransferReceiveBillDetails [str] ON stb.StoreTransferID=[str].StoreTransferID
+			LEFT OUTER JOIN
+			(
+				SELECT UserID,UserName FROM UserManagement WITH(NOLOCK)
+			) usr1 ON [str].CreatedBy=usr1.UserID
+			WHERE ISNULL([str].StoreTransferID,0) = ISNULL(@BillStatus,0)
+
+		END
+	
+	END
 
 	END TRY
 
--- IMS_Client_2_DB/Stores Procedures/SPR_Get_StoreTransfer_ListItems.sql
+++ IMS_Client_2_DB/Stores Procedures/SPR_Get_StoreTransfer_ListItems.sql
@@ -3,9 +3,9 @@
 -- Create date: <18th JULY 2020>
 -- Description:	<>
 -- =============================================
---EXEC SPR_Get_StoreTransfer_ListItems
-CREATE PROCEDURE SPR_Get_StoreTransfer_ListItems
-
+--EXEC SPR_Get_StoreTransfer_ListItems 1
+CREATE PROCEDURE [dbo].[SPR_Get_StoreTransfer_ListItems]
+@StoreBillDetailsID INT=0
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -13,11 +13,37 @@ BEGIN
 	SET NOCOUNT ON;
 
 	DECLARE @PARAMERES VARCHAR(MAX)=''
-	DECLARE @StoreID AS INT=0
+	DECLARE @ReceiveBillNo NVARCHAR(MAX)=''
+	DECLARE @TotalQTY AS INT=0
+	DECLARE @ReceiveBillDate AS DATE=NULL
 
 	BEGIN TRY
+	
+	SET @PARAMERES=@StoreBillDetailsID
 
-	SELECT @StoreID=StoreID FROM DefaultStoreSetting WITH(NOLOCK) WHERE MachineName=HOST_NAME()
+	SELECT 
+      @ReceiveBillNo=ReceiveBillNo
+      ,@TotalQTY=TotalQTY
+      ,@ReceiveBillDate=ReceiveBillDate
+  FROM tblStoreTransferReceiveBillDetails WITH(NOLOCK) WHERE StoreTransferID=@StoreBillDetailsID
+
+	SELECT st.TransferItemID,st.StoreBillDetailsID,st.Barcode,st.BillQTY,ISNULL(st.EnterQTY,0)EnterQTY
+	,'' [State],
+	 (CASE WHEN st.BillQTY = ISNULL(st.EnterQTY,0) THEN 'Green'
+	 WHEN st.BillQTY < ISNULL(st.EnterQTY,0) THEN 'Orange' ELSE 'Red' END) [CellColor]
+	,psm.ModelNo,@ReceiveBillNo [ReceiveBillNo],@TotalQTY [ReceivedTotalQTY],@ReceiveBillDate [ReceiveBillDate]
+	,st.ProductID,pm.ProductName [Item],cm.ColorName [Color],sm.Size
+	,stb.BillDate,stb.BillNo,stb.TotalQTY,st.Total,stm.StoreName
+	FROM tblStoreTransferItemDetails st
+	INNER JOIN [tblStoreTransferBillDetails] stb ON st.StoreBillDetailsID=stb.StoreTransferID
+	INNER JOIN ProductStockMaster psm ON st.ProductID=psm.ProductID AND st.ColorID=psm.ColorID AND st.SizeID=psm.SizeID 
+	AND psm.BarcodeNo IS NOT NULL AND psm.QTY > 0
+	
+	INNER JOIN ProductMaster pm ON st.ProductID=pm.ProductID
+	INNER JOIN ColorMaster cm ON st.ColorID=cm.ColorID
+	INNER JOIN SizeMaster sm ON st.SizeID=sm.SizeID
+	INNER JOIN StoreMaster stm ON stb.ToStore=stm.StoreID
+	WHERE st.StoreBillDetailsID=@StoreBillDetailsID
 
 	END TRY
 
@@ -45,4 +71,3 @@ BEGIN
 	END CATCH
 
 END
-GO

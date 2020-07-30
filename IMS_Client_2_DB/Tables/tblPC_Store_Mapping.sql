ba68562946f080ea7b12cabd484bc83d20d04857
Ashfaque <Ashfaque@ASHFAQUE>
2020-07-30 20:17:36 +0300

    added PC store mapping details

---
+++ IMS_Client_2_DB/Tables/tblPC_Store_Mapping.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblPC_Store_Mapping]
+(
+	[PC_Store_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [StoreID] INT NULL, 
+    [MachineName] NVARCHAR(50) NULL, 
+    [CreatedBy] [int] NOT NULL CONSTRAINT [DF_tblPC_Store_Mapping_CreatedBy]  DEFAULT ((0)),
+	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_tblPC_Store_Mapping_CreatedOn]  DEFAULT (getdate()),
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL,
+)

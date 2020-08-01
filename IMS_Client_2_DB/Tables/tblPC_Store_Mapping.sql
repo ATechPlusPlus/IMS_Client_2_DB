86a90c2bb634ee58fa7797f3f7113d4366e8ac6e
Ashfaque <Ashfaque@ASHFAQUE>
2020-08-01 15:43:27 +0300

    Edited column name of tblPC_Store_Mapping

--- IMS_Client_2_DB/Tables/tblPC_Store_Mapping.sql
+++ IMS_Client_2_DB/Tables/tblPC_Store_Mapping.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tblPC_Store_Mapping]
 (
 	[PC_Store_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [StoreID] INT NULL, 
+    [SM_StoreID] INT NULL, 
     [MachineName] NVARCHAR(50) NULL,
 	[SM_StoreCategory] INT NULL,
     [CreatedBy] [int] NOT NULL CONSTRAINT [DF_tblPC_Store_Mapping_CreatedBy]  DEFAULT ((0)),

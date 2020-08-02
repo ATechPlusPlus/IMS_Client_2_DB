34a5f2140802ce1e9cc7c1161c86a4ce42bbde5a
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-08-02 14:47:46 +0530

    Added column in GetProductDetails by barcode proc

--- IMS_Client_2_DB/Stores Procedures/GetProductDetailsByBarCode.sql
+++ IMS_Client_2_DB/Stores Procedures/GetProductDetailsByBarCode.sql
@@ -18,7 +18,7 @@ BEGIN
 	SET @PARAMERES=CONCAT(@StoreID,',',@BarCode)
 
 	SELECT p1.ProductID, p1.ProductName,p1.Rate,p2.QTY,p2.ColorID,c1.ColorName
-    ,s1.SizeID, s1.Size,p2.BarcodeNo FROM dbo.ProductMaster p1 
+    ,s1.SizeID, s1.Size,p2.BarcodeNo,p2.SubProductID FROM dbo.ProductMaster p1 
     JOIN dbo.ProductStockColorSizeMaster p2 ON p1.ProductID = p2.ProductID AND p2.StoreID = @StoreID
     JOIN ColorMaster c1 ON p2.colorID=c1.ColorID 
     JOIN SizeMaster s1 ON p2.SizeID=s1.SizeID

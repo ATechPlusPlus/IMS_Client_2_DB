2429a138d28061152ee42a69dbbf325c66e65479
AAMIR <AAMIR@KHAN>
2020-08-07 04:18:16 +0530

    1. Added SP SPR_Get_PCStoreMapping 2. Changes in column name in PCStoreMapping.

---
+++ IMS_Client_2_DB/Stores Procedures/SPR_Get_PCStoreMapping.sql
@@ -0,0 +1,47 @@
+﻿-- =============================================
+-- Author:		<AAMIR KHAN>
+-- Create date: <07th AUGUST 2020>
+-- Update date: <>
+-- Description:	<>
+-- =============================================
+--EXEC SPR_Get_PCStoreMapping
+CREATE PROCEDURE SPR_Get_PCStoreMapping
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	BEGIN TRY
+	DECLARE @PARAMERES VARCHAR(MAX)=''
+
+	SELECT PSM.PC_Store_ID,PSM.StoreID, PSM.MachineName, SM.StoreName,SM.StoreCategory [StoreCategoryID],
+	(CASE PSM.StoreCategory WHEN 1 THEN 'Warehouse' WHEN 0 THEN 'Normal Store' END) [StoreCategory] 
+	FROM [dbo].[tblPC_Store_Mapping] PSM 
+	INNER JOIN [dbo].[StoreMaster] SM ON SM.StoreID = PSM.StoreID
+	ORDER BY PSM.PC_Store_ID
+
+	END TRY
+
+	BEGIN CATCH
+	INSERT [dbo].[ERROR_Log]
+	(
+	ERR_NUMBER
+	, ERR_SEVERITY
+	, ERR_STATE
+	, ERR_LINE
+	, ERR_MESSAGE
+	, ERR_PROCEDURE
+	, PARAMERES
+	)
+	SELECT  
+	ERROR_NUMBER() 
+	,ERROR_SEVERITY() 
+	,ERROR_STATE() 
+	,ERROR_LINE()
+	,ERROR_MESSAGE()
+	,ERROR_PROCEDURE()
+	,@PARAMERES
+	
+	END CATCH
+END
+GO
--- IMS_Client_2_DB/Tables/tblPC_Store_Mapping.sql
+++ IMS_Client_2_DB/Tables/tblPC_Store_Mapping.sql
@@ -1,11 +1,16 @@
-﻿CREATE TABLE [dbo].[tblPC_Store_Mapping]
-(
-	[PC_Store_ID] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [SM_StoreID] INT NULL, 
-    [MachineName] NVARCHAR(50) NULL,
-	[SM_StoreCategory] INT NULL,
-    [CreatedBy] [int] NOT NULL CONSTRAINT [DF_tblPC_Store_Mapping_CreatedBy]  DEFAULT ((0)),
+﻿CREATE TABLE [dbo].[tblPC_Store_Mapping](
+	[PC_Store_ID] [int] IDENTITY(1,1) NOT NULL,
+	[StoreID] [int] NOT NULL,
+	[MachineName] [nvarchar](50) NOT NULL,
+	[StoreCategory] [int] NOT NULL,
+	[CreatedBy] [int] NOT NULL CONSTRAINT [DF_tblPC_Store_Mapping_CreatedBy]  DEFAULT ((0)),
 	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_tblPC_Store_Mapping_CreatedOn]  DEFAULT (getdate()),
 	[UpdatedBy] [int] NULL,
 	[UpdatedOn] [datetime] NULL,
-)
+ CONSTRAINT [PK__tblPC_St__A037AF7C64EDE3A7] PRIMARY KEY CLUSTERED 
+(
+	[PC_Store_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO

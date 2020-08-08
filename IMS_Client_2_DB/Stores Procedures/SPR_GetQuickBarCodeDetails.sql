e34bfa17cc4d06190905b6d7ca589a00deba0461
AAMIR <AAMIR@KHAN>
2020-08-08 11:33:18 +0530

    added Parameters variable to store input data in catch

--- IMS_Client_2_DB/Stores Procedures/SPR_GetQuickBarCodeDetails.sql
+++ IMS_Client_2_DB/Stores Procedures/SPR_GetQuickBarCodeDetails.sql
@@ -2,22 +2,21 @@
 -- =============================================
 -- Author:		<Mateen KHAN>
 -- Create date: <07th MARCH 2020>
--- Update date: <29th JULY 2020>
+-- Update date: <08th AUGUST 2020>
 -- Description:	<Description,,>
 -- =============================================
--- EXEC [dbo].[Get_PurchaseInvoice_BulkPrint_Color_Size] 2,3
+-- EXEC [dbo].[SPR_GetQuickBarCodeDetails] 1000
 CREATE PROCEDURE [dbo].[SPR_GetQuickBarCodeDetails]
-@BarCodeNo as nvarchar(50)
+@BarCodeNo NVARCHAR(MAX)
 
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	SET NOCOUNT ON;
-	DECLARE @PARAMERES VARCHAR(MAX)=''
-	SET @PARAMERES=@BarCodeNo
 
 	BEGIN TRY
-
+	DECLARE @PARAMERES VARCHAR(MAX)=''
+	SET @PARAMERES=@BarCodeNo
 
 	SELECT ps.ProductStockID, ps.PurchaseInvoiceID, ps.SubProductID, ps.ProductID,pm.ProductName,ps.Rate
 	, ps.StoreID, ps.BarcodeNo, ps.ColorID,cm.ColorName AS Color,sm.SizeID,sm.Size, ps.QTY, ps.ModelNo

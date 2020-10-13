0bacc2a5dc7ec25e0b298336b8baff66c21c22f6
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-10-13 22:28:28 +0530

    Inventory DB changes...

---
+++ IMS_Client_2_DB/Tables/tblScanInventoryDetails.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[tblScanInventoryDetails](
+	[MasterScanID] [int] IDENTITY(1,1) NOT NULL,
+	 [ScanDate] DATE NULL, 
+	[StoreID] [int] NOT NULL,
+	[CompareStatus] BIT NOT NULL,
+	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_tblScanInventory_CreatedOn]  DEFAULT (getdate()),
+	[CreatedBy] [int] NOT NULL DEFAULT 0,
+	[UpdatedOn] [datetime] NULL,
+	[UpdatedBy] [int] NULL,
+   
+    CONSTRAINT [PK_tblScanInventory] PRIMARY KEY CLUSTERED 
+(
+	[MasterScanID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
---
+++ IMS_Client_2_DB/Tables/tblScanInventoryItemDetails.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[tblScanInventoryItemDetails]
+(
+[AutoID] [int] IDENTITY(1,1) NOT NULL,
+	[ScanInventoryID] [int] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[SubProductID] [int] NOT NULL,
+	[Barcode] [nvarchar](50) NOT NULL,
+	[Rate] [decimal](18, 3) NOT NULL,
+	[BillQTY] [int] NOT NULL,
+	[EnterQTY] [int] NOT NULL DEFAULT 0,
+	[ColorID] [int] NOT NULL,
+	[SizeID] [int] NOT NULL,
+	[Total] [decimal](18, 3) NULL,
+	 [SystemQTY] INT NULL, 
+	
+	[CreatedOn] [datetime] NOT NULL CONSTRAINT [DF_tblScanInventoryItemDetails_CreatedOn]  DEFAULT (getdate()),
+	[CreatedBy] [int] NOT NULL CONSTRAINT [DF_tblScanInventoryItemDetails_CreatedBy]  DEFAULT ((0)),
+	[UpdatedBy] [int] NULL,
+	[UpdatedOn] [datetime] NULL
+
+    CONSTRAINT [PK_tblScanInventoryItemDetails] PRIMARY KEY CLUSTERED 
+(
+	[AutoID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO

d15b5bc2932f182adc568618a36ff47acecc878b
AAMIR <khan89aamir@gmail.com>
2021-03-22 01:03:07 +0530

    1.Added SP SPR_Get_ItemCard_Material_Details 2. Added SP SPR_Get_ItemCard_Details

---
+++ IMS_Client_2_DB/Stores Procedures/SPR_Get_ItemCard_Details.sql
@@ -0,0 +1,105 @@
+﻿-- =============================================
+-- Author:		<AAMIR KHAN>
+-- Create date: <21th MARCH 2021>
+-- Update date: <>
+-- Description:	<Description,,>
+-- =============================================
+--EXEC SPR_Get_ItemCard_Details 1232,'0'
+--EXEC SPR_Get_ItemCard_Details 0,'A1641'
+CREATE PROCEDURE [dbo].[SPR_Get_ItemCard_Details]
+@BarCode BIGINT=0
+,@ModelNo NVARCHAR(MAX)='0'
+--,@ProductID INT=0
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	BEGIN TRY
+	DECLARE @PARAMERES VARCHAR(MAX)=''
+	DECLARE @InvoiceDate DATE='1900-01-01'
+	DECLARE @SoldQTY INT=0
+
+	SET @PARAMERES=CONCAT(@BarCode,',',@ModelNo)
+
+	SET NOCOUNT ON;
+
+	IF @ModelNo = '0'
+	BEGIN
+
+		SELECT @InvoiceDate=CONVERT(DATE,MAX(si.InvoiceDate)) ,@SoldQTY=SUM(sd.QTY)
+		FROM SalesInvoiceDetails si
+		INNER JOIN View_SalesDetails sd ON si.Id=sd.InvoiceID
+		WHERE sd.BarcodeNo=@BarCode
+
+			SELECT t.SubProductID,t.ProductID,t.[Item Name],t.[Style No],SUM(t.QTY) QTY
+			,t.EndUser,t.LocalCost,@InvoiceDate InvoiceDate,ISNULL(@SoldQTY,0) SoldQTY
+			FROM
+			(
+			SELECT CAST(ISNULL(ps.BarcodeNo,0) AS VARCHAR)BarcodeNo,ps.SubProductID
+			,cm.ColorName[Color],sz.Size,pwm.ModelNo [Style No],pm.ProductID,pm.ProductName [Item Name]
+			,CAST(pwm.EndUser AS VARCHAR)[EndUser],pwm.LocalCost,pwm.Photo,ps.QTY
+			FROM ProductStockColorSizeMaster ps
+			INNER JOIN ProductMaster pm ON ps.ProductID=pm.ProductID
+			INNER JOIN CategoryMaster cat ON pm.CategoryID=cat.CategoryID
+			INNER JOIN StoreMaster sm ON ps.StoreID=sm.StoreID
+			INNER JOIN ColorMaster cm ON ps.ColorID=cm.ColorID
+			INNER JOIN SizeMaster sz ON ps.SizeID=sz.SizeID
+			INNER JOIN tblProductWiseModelNo pwm ON ps.SubProductID=pwm.SubProductID AND ps.ProductID=pwm.ProductID
+			)t
+			WHERE t.BarcodeNo = @BarCode
+			GROUP BY t.SubProductID,t.ProductID,t.[Item Name],t.[Style No],t.EndUser,t.LocalCost
+
+	END
+
+	ELSE
+	BEGIN
+	
+			SELECT TOP 1 @InvoiceDate=CONVERT(DATE,MAX(si.InvoiceDate)) ,@SoldQTY=SUM(sd.QTY)
+			FROM SalesInvoiceDetails si
+			INNER JOIN View_SalesDetails sd ON si.Id=sd.InvoiceID
+			WHERE sd.ModelNo LIKE ''+@ModelNo+'%'
+
+			SELECT t.SubProductID,t.ProductID,t.[Item Name],t.[Style No],SUM(t.QTY) QTY
+			,t.EndUser,t.LocalCost,@InvoiceDate InvoiceDate,ISNULL(@SoldQTY,0) SoldQTY
+			FROM
+			(
+			SELECT CAST(ISNULL(ps.BarcodeNo,0) AS VARCHAR)BarcodeNo,ps.SubProductID
+			,cm.ColorName[Color],sz.Size,pwm.ModelNo [Style No],pm.ProductID,pm.ProductName [Item Name]
+			,CAST(pwm.EndUser AS VARCHAR)[EndUser],pwm.LocalCost,pwm.Photo,ps.QTY
+			FROM ProductStockColorSizeMaster ps
+			INNER JOIN ProductMaster pm ON ps.ProductID=pm.ProductID
+			INNER JOIN CategoryMaster cat ON pm.CategoryID=cat.CategoryID
+			INNER JOIN StoreMaster sm ON ps.StoreID=sm.StoreID
+			INNER JOIN ColorMaster cm ON ps.ColorID=cm.ColorID
+			INNER JOIN SizeMaster sz ON ps.SizeID=sz.SizeID
+			INNER JOIN tblProductWiseModelNo pwm ON ps.SubProductID=pwm.SubProductID AND ps.ProductID=pwm.ProductID
+			)t
+			WHERE t.[Style No] LIKE ''+@ModelNo+'%'
+			GROUP BY t.SubProductID,t.ProductID,t.[Item Name],t.[Style No],t.EndUser,t.LocalCost
+
+	END
+
+	END TRY
+
+	BEGIN CATCH
+	INSERT [dbo].[ERROR_Log]
+	(
+	ERR_NUMBER
+	, ERR_SEVERITY
+	, ERR_STATE
+	, ERR_LINE
+	, ERR_MESSAGE
+	, ERR_PROCEDURE
+	, PARAMERES
+	)
+	SELECT  
+	ERROR_NUMBER() 
+	,ERROR_SEVERITY() 
+	,ERROR_STATE() 
+	,ERROR_LINE()
+	,ERROR_MESSAGE()
+	,ERROR_PROCEDURE()
+	,@PARAMERES
+	
+	END CATCH
+END
---
+++ IMS_Client_2_DB/Stores Procedures/SPR_Get_ItemCard_Material_Details.sql
@@ -0,0 +1,143 @@
+﻿-- =============================================
+-- Author:		<AAMIR KHAN>
+-- Create date: <21st MAR 2021>
+-- Update date: <>
+-- Description:	<>
+-- =============================================
+--EXEC SPR_Get_ItemCard_Material_Details 0,'CRO-B2'
+CREATE PROCEDURE [dbo].[SPR_Get_ItemCard_Material_Details]
+@BarCode	BIGINT=0
+,@ModelNo	NVARCHAR(MAX)
+--,@ProductID INT=0
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+		BEGIN TRY
+
+		DECLARE @PARAMERES NVARCHAR(MAX)=''
+		DECLARE @query1 NVARCHAR(MAX)=''
+		DECLARE @query2 NVARCHAR(MAX)=''
+		DECLARE @queryEndUser NVARCHAR(MAX)=''
+		
+		DECLARE @SumOfQTY NVARCHAR(MAX)=''
+		DECLARE @SumOfQTYEndUser NVARCHAR(MAX)=''
+		DECLARE @TotalStoreQuery1 NVARCHAR(MAX)=''
+		DECLARE @WHERE VARCHAR(MAX)=''
+		
+		DECLARE @Comma NVARCHAR(MAX)=','
+		DECLARE @StoreID INT=0
+		DECLARE @StoreName NVARCHAR(MAX)=''
+
+		SET @PARAMERES=CONCAT(@BarCode,',',@ModelNo)
+		--BEGIN TRANSACTION
+
+			DECLARE cursor_Store CURSOR
+			FOR
+			
+			SELECT StoreID,StoreName 
+			FROM StoreMaster WITH(NOLOCK) WHERE ISNULL(ActiveStatus,0) = 1
+	
+			OPEN cursor_Store;
+
+			FETCH NEXT FROM cursor_Store INTO @StoreID,@StoreName
+
+				WHILE @@FETCH_STATUS <> -1
+				BEGIN
+
+				SET @query1 += 'ISNULL(MAX(CASE sm.StoreID WHEN '+CAST(@StoreID AS VARCHAR)+
+				' THEN ps.QTY END),0) '+QUOTENAME(@StoreName)+@Comma;
+
+				SET @queryEndUser+='ISNULL(MAX(CASE sm.StoreID WHEN '+CAST(@StoreID AS VARCHAR)+
+				' THEN ps.QTY * pwm.EndUser END),0) '+QUOTENAME(CONCAT(@StoreName,' Rate'))+@Comma
+				
+				SET @SumOfQTY +='SUM(pt.'+QUOTENAME(@StoreName)+') '+QUOTENAME(@StoreName)+@Comma
+
+				SET @SumOfQTYEndUser+='SUM(pt.'+QUOTENAME(CONCAT(@StoreName,' Rate'))+') '+QUOTENAME(CONCAT(@StoreName,' Rate'))+@Comma
+
+				IF @@FETCH_STATUS <> -1 BEGIN
+				SET @Comma=''
+				END
+
+				FETCH NEXT FROM cursor_Store INTO @StoreID,@StoreName
+
+				END
+
+			CLOSE cursor_Store;
+			DEALLOCATE cursor_Store;
+			
+			
+				SET @query1+=',ISNULL(SUM(ps.QTY),0) AS Total'
+				SET @SumOfQTY+=',SUM(pt.Total) [Total]'
+			
+
+			SET @WHERE='WHERE ISNULL(ps.BarcodeNo,0)=IIF('+CAST(@BarCode AS VARCHAR)+'=0,ISNULL(ps.BarcodeNo,0),'
+			+CAST(@BarCode AS VARCHAR)+')
+			AND pwm.ModelNo LIKE IIF('''+CAST(@ModelNo AS NVARCHAR)+'''=''0'',pwm.ModelNo+''%'','''+CAST(@ModelNo AS NVARCHAR)+'%'')
+			--AND pwm.ModelNo=IIF('''+CAST(@ModelNo AS NVARCHAR)+'''=''0'',pwm.ModelNo,'''+CAST(@ModelNo AS NVARCHAR)+''')
+			GROUP BY ps.BarcodeNo,cm.ColorName,sz.Size,pm.ProductName,pm.ProductID,pwm.EndUser,pwm.Photo,ps.SubProductID,pwm.ModelNo'
+
+			SET @query2='SELECT ps.SubProductID
+			,cm.ColorName [Color],sz.Size,pwm.ModelNo [Style No],pm.ProductID,pm.ProductName [Item Name],
+			'+@query1+'
+			FROM ProductStockColorSizeMaster ps
+			INNER JOIN ProductMaster pm ON ps.ProductID=pm.ProductID
+			INNER JOIN CategoryMaster cat ON pm.CategoryID=cat.CategoryID
+			INNER JOIN StoreMaster sm ON ps.StoreID=sm.StoreID
+			INNER JOIN ColorMaster cm ON ps.ColorID=cm.ColorID
+			INNER JOIN SizeMaster sz ON ps.SizeID=sz.SizeID
+			INNER JOIN tblProductWiseModelNo pwm ON ps.SubProductID=pwm.SubProductID AND ps.ProductID=pwm.ProductID
+			'+@WHERE+'
+			'
+			--SET @query2=REPLACE(@query2,',
+			--[Remove]','')
+
+			--print @query2
+
+			SET @TotalStoreQuery1='UNION 
+			SELECT '''' SubProductID,CAST(''Total'' AS VARCHAR) [Color],'''' [Size],
+			'''' [Style No],'''' [ProductID],'''' [Item Name],'+@SumOfQTY+'
+			FROM (SELECT '+@query1+' 
+			FROM ProductStockColorSizeMaster ps
+			INNER JOIN ProductMaster pm ON ps.ProductID=pm.ProductID
+			INNER JOIN CategoryMaster cat ON pm.CategoryID=cat.CategoryID
+			INNER JOIN StoreMaster sm ON ps.StoreID=sm.StoreID
+			INNER JOIN ColorMaster cm ON ps.ColorID=cm.ColorID
+			INNER JOIN SizeMaster sz ON ps.SizeID=sz.SizeID
+			INNER JOIN tblProductWiseModelNo pwm ON ps.SubProductID=pwm.SubProductID AND ps.ProductID=pwm.ProductID
+			'+@WHERE+'
+			)pt'
+			
+			--PRINT @query2 + @TotalStoreQuery1
+			EXEC (@query2 + @TotalStoreQuery1)
+			 
+			END TRY
+	
+			BEGIN CATCH
+
+			INSERT [dbo].[ERROR_Log]
+			(
+			ERR_NUMBER
+			, ERR_SEVERITY
+			, ERR_STATE
+			, ERR_LINE
+			, ERR_MESSAGE
+			, ERR_PROCEDURE
+			, PARAMERES
+			)
+			SELECT  
+			ERROR_NUMBER() 
+			,ERROR_SEVERITY() 
+			,ERROR_STATE() 
+			,ERROR_LINE()
+			,ERROR_MESSAGE()
+			,ISNULL(ERROR_PROCEDURE(),'SPR_Get_ItemCard_Material_Details')
+			,@PARAMERES
+
+			SELECT -1 AS Flag,ERROR_MESSAGE() AS Msg
+
+			END CATCH
+	
+			END

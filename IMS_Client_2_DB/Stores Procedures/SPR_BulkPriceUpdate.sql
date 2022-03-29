a52659a1148b8c1fe103ebe31a9747101e63789e
AAMIR <khan89aamir@gmail.com>
2022-03-29 19:28:10 +0530

    Changed in SP SPR_BulkPriceUpdate for updating respective tables in bulk if all records are avaialble then update.

--- IMS_Client_2_DB/Stores Procedures/SPR_BulkPriceUpdate.sql
+++ IMS_Client_2_DB/Stores Procedures/SPR_BulkPriceUpdate.sql
@@ -15,37 +15,45 @@ BEGIN
 
 	BEGIN TRY
 	DECLARE @PARAMERES VARCHAR(MAX)=''
-	SET @PARAMERES=@LoginBy
+	DECLARE @rows INT=0
+
+	SELECT @rows=COUNT(1) FROM @ExcelTable
+	SET @PARAMERES=CONCAT(@LoginBy,', Total Rows ',@rows)
 
 	BEGIN TRANSACTION
-
-		UPDATE pwm
-		SET pwm.EndUser=t3.SalePrice,pwm.UpdatedOn=GETDATE(),pwm.UpdatedBy=@LoginBy
-		FROM tblProductWiseModelNo AS pwm
-		INNER JOIN View_ModeBrandDetails AS t2 ON pwm.SubProductID=t2.SubProductID
-		INNER JOIN @ExcelTable AS t3 ON t2.BrandName=t3.Brand AND t2.ModelNo=t3.StyleNo
-
-		UPDATE psm
-		SET psm.Rate=t3.SalePrice,psm.UpdatedOn=GETDATE(),psm.UpdatedBy=@LoginBy
-		FROM ProductStockMaster AS psm
-		INNER JOIN View_ModeBrandDetails AS t2 ON psm.SubProductID=t2.SubProductID
-		INNER JOIN @ExcelTable AS t3 ON t2.BrandName=t3.Brand AND t2.ModelNo=t3.StyleNo
-
-		--UPDATE ProductStockMaster
-		-- SET ProductStockMaster.Rate=t3.SalePrice,ProductStockMaster.UpdatedOn=GETDATE(),ProductStockMaster.UpdatedBy=@LoginBy
-		--FROM ProductStockMaster AS t1 JOIN (SELECT * FROM  View_ModeBrandDetails ) AS t2
-		--ON t1.SubProductID=t2.SubProductID  JOIN 
-		--@ExcelTable AS t3 ON (t2.BrandName=t3.Brand AND t2.ModelNo=t3.StyleNo)
-
-		COMMIT
 		
 		SELECT @Flag=COUNT(1) FROM tblProductWiseModelNo AS pwm
 		INNER JOIN View_ModeBrandDetails AS t2 ON pwm.SubProductID=t2.SubProductID
 		INNER JOIN @ExcelTable AS t3 ON t2.BrandName=t3.Brand AND t2.ModelNo=t3.StyleNo
 
+		IF @rows=@Flag 
+		BEGIN
+			UPDATE pwm
+			SET pwm.EndUser=t3.SalePrice,pwm.UpdatedOn=GETDATE(),pwm.UpdatedBy=@LoginBy
+			FROM tblProductWiseModelNo AS pwm
+			INNER JOIN View_ModeBrandDetails AS t2 ON pwm.SubProductID=t2.SubProductID
+			INNER JOIN @ExcelTable AS t3 ON t2.BrandName=t3.Brand AND t2.ModelNo=t3.StyleNo
+
+			UPDATE psm
+			SET psm.Rate=t3.SalePrice,psm.UpdatedOn=GETDATE(),psm.UpdatedBy=@LoginBy
+			FROM ProductStockMaster AS psm
+			INNER JOIN View_ModeBrandDetails AS t2 ON psm.SubProductID=t2.SubProductID
+			INNER JOIN @ExcelTable AS t3 ON t2.BrandName=t3.Brand AND t2.ModelNo=t3.StyleNo
+
+			--UPDATE ProductStockMaster
+			-- SET ProductStockMaster.Rate=t3.SalePrice,ProductStockMaster.UpdatedOn=GETDATE(),ProductStockMaster.UpdatedBy=@LoginBy
+			--FROM ProductStockMaster AS t1 JOIN (SELECT * FROM  View_ModeBrandDetails ) AS t2
+			--ON t1.SubProductID=t2.SubProductID  JOIN 
+			--@ExcelTable AS t3 ON (t2.BrandName=t3.Brand AND t2.ModelNo=t3.StyleNo)
+
+		SET @Flag=1;
+		END
+		COMMIT
+		
 	END TRY
 
 BEGIN CATCH
+	SET @Flag=0;
 	INSERT [dbo].[ERROR_Log]
 	(
 	ERR_NUMBER
